86a979f9b9583b13aa3b5fa979997eb692d20943
JiaY-shi <shi05275@163.com>
2023-06-17 17:03:04 +0800

    QualcommAX: ipq60xx: add support for GL.iNet gl-ax1800 and gl-axt1800

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -51,6 +51,8 @@ platform_do_upgrade() {
 		fw_setenv auto_recovery yes
 		nand_do_upgrade "$1"
 		;;
+	glinet,gl-axt1800|\
+	glinet,gl-ax1800|\
 	netgear,wax214|\
 	qihoo,360v6)
 		nand_do_upgrade "$1"
