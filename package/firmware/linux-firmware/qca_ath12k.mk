da7d5f21d5be5d99ef4a44b6f7ac3f53d3bead05
JiaY-shi <shi05275@163.com>
2026-09-21 22:25:38 +0800

    firmware: update QCN9274 ath12k firmware

--- package/firmware/linux-firmware/qca_ath12k.mk
+++ package/firmware/linux-firmware/qca_ath12k.mk
@@ -9,9 +9,14 @@ endef
 $(eval $(call BuildPackage,ath12k-firmware-wcn7850))
 
 Package/ath12k-firmware-qcn9274 = $(call Package/firmware-default,QCN9274 ath12k firmware)
+# Keep QCN9274 on the same WLAN.WBE build as the bundled IPQ5332 firmware.
+# Source: mirror/dd-wrt, WLAN.WBE.1.6-01270-QCAHKSWPL_SILICONZ-1.
 define Package/ath12k-firmware-qcn9274/install
 	$(INSTALL_DIR) $(1)/lib/firmware/ath12k/QCN9274/hw2.0
 	$(INSTALL_DATA) \
 		$(PKG_BUILD_DIR)/ath12k/QCN9274/hw2.0/* $(1)/lib/firmware/ath12k/QCN9274/hw2.0/
+	$(INSTALL_DATA) \
+		$(CURDIR)/files/QCN9274/hw2.0/firmware-2.bin \
+		$(1)/lib/firmware/ath12k/QCN9274/hw2.0/firmware-2.bin
 endef
 $(eval $(call BuildPackage,ath12k-firmware-qcn9274))
